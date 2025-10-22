c259995b1961221e24e43f3162981c7164a96a80
Jacob Tietz <jaetietz@gmail.com>
2025-10-22 13:28:29 -0500

    CookieTokens

--- src/api.js
+++ src/api.js
@@ -4,4 +4,5 @@ const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
 export const api = axios.create({
   baseURL: API_URL,
+  withCredentials: true, // <--- crucial for sending cookies cross-site
 });
