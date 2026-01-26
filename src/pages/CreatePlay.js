c7ec515444091e65b4ec3b11eacaca33a9196cc5
Jacob Tietz <jaetietz@gmail.com>
2026-01-25 20:18:22 -0600

    Update options in CreatePlay.js
    Removed 'Professional' option from education select and added 'Reader’s Theatre' and 'Monologue' options to genre select.

--- src/pages/CreatePlay.js
+++ src/pages/CreatePlay.js
@@ -296,7 +296,6 @@ export default function CreatePlay() {
             <option value="High School">High School</option>
             <option value="University">University</option>
             <option value="Community">Community</option>
-            <option value="Professional">Professional</option>
           </select>
 
           <label>Genre</label>
@@ -309,7 +308,8 @@ export default function CreatePlay() {
             <option value="Comedy">Comedy</option>
             <option value="Musical">Musical</option>
             <option value="Drama">Drama</option>
-            <option value="Theatre of the Mind">Theatre of the Mind</option>
+            <option value="Reader’s Theatre">Reader’s Theatre</option>
+            <option value="Monologue">Monologue</option>
             <option value="Tragedy">Tragedy</option>
             <option value="Mystery">Mystery</option>
           </select>
